bc5acdaf6aa5d9bde1b1cab539c9082571940e54
lidmila <lidmilamarsalkova@gmail.com>
2017-01-07 19:03:12 +0100

    Změna vše o nákupu, barev a patičky

--- webpack/src/SelectedBox.js
+++ webpack/src/SelectedBox.js
@@ -14,16 +14,14 @@ export default observer(class SelectedBox extends React.Component {
       <div>
         <div className="row">
           <div className="col-lg-6 col-lg-offset-4">
-            <h4>VYBRAL JSTE SI KRABIČKU</h4>
             <h4>{product}</h4>
             <ul dangerouslySetInnerHTML={{__html: productDescription}}></ul>
-            <h4>CENA</h4>
             <p>
-              {price} Kč
+            <strong>Cena krabičky:</strong> {price} Kč
             </p>
             <a href="/hotove-krabicky.html" className="btn btn-default">Změnit výběr</a>
             &nbsp;
-            <button onClick={this.cancelUserSelectedBox} className="btn btn-danger btn-sm">Chci si vytvořit vlastní krabičku</button>
+            <button onClick={this.cancelUserSelectedBox} className="btn btn-default">Chci si vytvořit vlastní krabičku</button>
           </div>
         </div>
       </div>
