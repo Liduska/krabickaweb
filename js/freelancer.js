6057249ec1952c810f1ebcf515e337870cf636a1
Josef Rousek <josef.rousek@gmail.com>
2017-01-07 14:30:32 +0100

    Change selector to be more picky

--- js/freelancer.js
+++ js/freelancer.js
@@ -1,10 +1,8 @@
-// Freelancer Theme JavaScript
-
 (function($) {
-    "use strict"; // Start of use strict
+    "use strict";
 
     // jQuery for page scrolling feature - requires jQuery Easing plugin
-    $('.page-scroll a').bind('click', function(event) {
+    $('[href="#contact"], [href="#order"]').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: ($($anchor.attr('href')).offset().top - 50)
@@ -30,4 +28,4 @@
         }
     })
 
-})(jQuery); // End of use strict
+})(jQuery);
