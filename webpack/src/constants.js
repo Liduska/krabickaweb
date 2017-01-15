9cbb391c76ba2137b71a35ce1be927b3fe88b1ed
lidmila <lidmilamarsalkova@gmail.com>
2017-01-15 13:40:24 +0100

    Popisky ke krabičkám

--- webpack/src/constants.js
+++ webpack/src/constants.js
@@ -1,9 +1,9 @@
 
 export const PRICES = [
-  { name: 'Radostná', price: 700, description: 'Krabička z drobných radostí může obsahovat např. plecháček, bio kosmetiku, sypaný čaj, vonné svíčky, diář apod. Naše nejlevnější krabička jistě udělá radost komukoliv, komu ji věnujete.' },
-  { name: 'Sváteční', price: 1000, description: 'Vybavte svou sestru nebo přítelkyni stylovou plátěnou taškou nebo třeba ručně vyrobenými náušnicemi! Tahle sváteční krabička je pro ženy jako stvořená.' },
+  { name: 'Radostná', price: 700, description: 'Krabička z drobných radostí pro každý den. Obsahuje levné dárky, které rozhodně nezklamou kvalitou, ani originalitou, např. plecháček, bio kosmetiku, diář, vonné svíčky apod.' },
+  { name: 'Sváteční', price: 1000, description: 'Ukrývá nejen dárky k svátku, ale pro každou příležitost. Vždyť zrnková káva z čerstvé pražírny a k tomu termohrnek na kafe mohou udělat radost kdykoliv a komukoliv.' },
   { name: 'Důležitá', price: 1500, description: 'Vše důležité pro důležité. Po otevření krabičky na Vás vykoukne např. nový vázací motýlek, otvírák na víno anebo skleněný termohrnek. Zkrátka samé důležité věci!' },
-  { name: 'Výjimečná', price: 2000, description: 'Najít dárek k výročí je stejně náročné jako výšlap na Mount Everest. Nebo ne? Výjimečná krabička Vám to usnadní a nabídne vše možné od parfémů po karafu na víno.' },
-  { name: 'VIP', price: 2500, description: 'Psaníčko na párty pro městskou kočku nebo ručně šitý pléd pro hřejivé večery. Nejen to si zaslouží každá VIP osoba ve Vašem životě. Obsah krabičky je zkrátka dokonale rozmazlí.' },
-  { name: 'Královská', price: 3000, description: 'Pro opravdu důležité momenty. Královská krabička nabídne luxusní kosmetiku i produkty z pravé kůže. Vždyť nejde jen o originalitu, ale také o kvalitu. ' },
+  { name: 'Výjimečná', price: 2000, description: 'Najít dárek k výročí je stejně náročné jako výšlap na Mount Everest. Nebo ne? Výjimečná krabička Vám to usnadní a nabídne velmi speciální dárky, např. svůdný parfém nebo dřevěného motýlka.' },
+  { name: 'VIP', price: 2500, description: 'Psaníčko na párty pro městskou kočku nebo ručně šitý pléd pro hřejivé večery. Nejen to si zaslouží každá VIP osoba ve Vašem životě. Dárky v krabičce je zkrátka dokonale rozmazlí.' },
+  { name: 'Královská', price: 3000, description: 'Pro opravdu důležité momenty. Královská krabička pro Vás ukrývá velmi luxusní dárky. Co říkáte např. na luxusní skleničky na víno se Swarovski krystaly? Vždyť nejde jen o originalitu, ale také o kvalitu. ' },
 ]
