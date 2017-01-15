98e9000ed67590093072219436f3ab96ed750d6e
lidmila <lidmilamarsalkova@gmail.com>
2017-01-15 18:47:43 +0100

    Úprava textu

--- webpack/src/constants.js
+++ webpack/src/constants.js
@@ -1,8 +1,8 @@
 
 export const PRICES = [
   { name: 'Radostná', price: 700, description: 'Krabička z drobných radostí pro každý den. Obsahuje levné dárky, které rozhodně nezklamou kvalitou, ani originalitou, např. plecháček, bio kosmetiku, diář, vonné svíčky apod.' },
-  { name: 'Sváteční', price: 1000, description: 'Ukrývá nejen dárky k svátku, ale pro každou příležitost. Vždyť zrnková káva z čerstvé pražírny a k tomu termohrnek na kafe mohou udělat radost kdykoliv a komukoliv.' },
-  { name: 'Důležitá', price: 1500, description: 'Vše důležité pro důležité. Po otevření krabičky na Vás vykoukne např. nový vázací motýlek, otvírák na víno anebo skleněný termohrnek. Zkrátka samé důležité věci!' },
+  { name: 'Sváteční', price: 1000, description: 'Ukrývá nejen dárky k svátku, ale pro každou příležitost. Vždyť zrnková káva z čerstvé pražírny a k tomu skleněný termohrnek na kafe mohou udělat radost kdykoliv a komukoliv.' },
+  { name: 'Důležitá', price: 1500, description: 'Vše důležité pro důležité. Po otevření krabičky na Vás vykoukne např. nový vázací motýlek, otvírák na víno anebo malá kabelka do města. Zkrátka samé důležité věci!' },
   { name: 'Výjimečná', price: 2000, description: 'Najít dárek k výročí je stejně náročné jako výšlap na Mount Everest. Nebo ne? Výjimečná krabička Vám to usnadní a nabídne velmi speciální dárky, např. svůdný parfém nebo dřevěného motýlka.' },
   { name: 'VIP', price: 2500, description: 'Psaníčko na párty pro městskou kočku nebo ručně šitý pléd pro hřejivé večery. Nejen to si zaslouží každá VIP osoba ve Vašem životě. Dárky v krabičce je zkrátka dokonale rozmazlí.' },
   { name: 'Královská', price: 3000, description: 'Pro opravdu důležité momenty. Královská krabička ukrývá velmi luxusní dárky. Co říkáte např. na elegantní skleničky na víno se Swarovski krystaly nebo hodinky? Vždyť nejde jen o originalitu, ale také o kvalitu. ' },
