4c5ff3a46a7810e855ce7692df4ab176c31da134
Travis-CI <travis@krabickanamiru.cz>
2017-03-19 12:15:51 +0000

    Deployed to Github Pages

---
+++ js/freelancer.js
@@ -0,0 +1,25 @@
+(function($) {
+    "use strict";
+
+    // jQuery for page scrolling feature - requires jQuery Easing plugin
+    $('[href="#contact"], [href="#page-top"]').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+        }, 1250, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+    // Closes the Responsive Menu on Menu Item Click
+    $('.navbar-collapse ul li a').click(function(){
+            $('.navbar-toggle:visible').click();
+    });
+
+    // Offset for Main Navigation
+    $('#mainNav').affix({
+        offset: {
+            top: 100
+        }
+    })
+
+})(jQuery);
